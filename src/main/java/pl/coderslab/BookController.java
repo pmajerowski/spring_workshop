ebb631840d32aac970a56be50ee619a37f7a6fe5
pmajerowski <przemyslaw.majerowski@gmail.com>
2023-02-03 14:52:44 +0100

    in BooksController at the bottom added in comments basic curl formulas for testing

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -43,3 +43,9 @@ public class BookController {
     }
 
 }
+
+/*
+    curl -X POST -i -H "Content-Type: application/json" -d '{"isbn":"1100294652","title":"The Road", "publisher":"Reading World","type":"science fiction", "author":"Cormac McCarthy"}' http://localhost:8080/books
+    curl -X DELETE -i  http://localhost:8080/books/2
+    curl -X PUT -i -H "Content-Type: application/json" -d '{"id":3,"isbn":"1122233","title":"Old Man And The Sea",  "publisher":"World Industries","type":"novel", "author":"Ernest Hemingway"}' http://localhost:8080/books
+ */
